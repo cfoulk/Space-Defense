a09acf4a65ab588d8dbf1d20c061456a389f960a
cfoulk <charlesfoulk@csus.edu>
2020-04-05 09:18:45 -0700

    Initial commit

---
+++ app/src/main/java/com/project/spacedefense/GameActivity.java
@@ -0,0 +1,56 @@
+package com.project.spacedefense;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+
+import java.io.IOException;
+
+
+/*
+This is completely copied over from previous projects only changed some names
+*/
+
+
+public class GameActivity extends Activity {
+
+    TowerGame tGame;
+
+    // Set the game up
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get the pixel dimensions of the screen
+        Display display = getWindowManager().getDefaultDisplay();
+
+        // Initialize the result into a Point object
+        Point size = new Point();
+        display.getSize(size);
+
+        // Create a new instance of the TowerGame class
+        try {
+            tGame = new TowerGame(this, size);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Make Engine the view of the Activity
+        setContentView(tGame);
+    }
+
+    // Start the thread in snakeEngine
+    @Override
+    protected void onResume() {
+        super.onResume();
+        tGame.resume();
+    }
+
+    // Stop the thread in snakeEngine
+    @Override
+    protected void onPause() {
+        super.onPause();
+        tGame.pause();
+    }
+}
---
+++ app/src/main/java/com/project/spacedefense/TowerGame.java
@@ -0,0 +1,4 @@
+package com.project.spacedefense;
+
+public class TowerGame {
+}
